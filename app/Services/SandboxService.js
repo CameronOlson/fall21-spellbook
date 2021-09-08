b1ca00e14fcb82d632859d1c78ed1516d1a551a5
Jake Overall <joverall22@gmail.com>
2021-09-08 12:54:01 -0600

    student handoff

---
+++ app/Services/SandboxService.js
@@ -0,0 +1,32 @@
+import { ProxyState } from "../AppState.js"
+import { SandboxSpell } from "../Models/SandboxSpell.js"
+
+// @ts-ignore
+const sandboxApi = axios.create({
+  baseURL: 'https://bcw-sandbox.herokuapp.com/api/'
+})
+
+class SandboxService {
+  async togglePrepared(spellId) {
+    // find the spell
+    const spell = ProxyState.sandboxSpells.find(s => s.id === spellId)
+    // update the spell
+    spell.prepared = !spell.prepared
+    await sandboxApi.put(`${ProxyState.user}/spells/${spellId}`, spell)
+  }
+  async learnSpell() {
+
+    const spellToLearn = new SandboxSpell(ProxyState.currentSpell)
+    let res = await sandboxApi.post(`${ProxyState.user}/spells`, spellToLearn)
+    ProxyState.sandboxSpells = [...ProxyState.sandboxSpells, new SandboxSpell(res.data)]
+  }
+
+  async getSpells() {
+    let res = await sandboxApi.get(`${ProxyState.user}/spells`)
+    console.log('log the res!!!!', res)
+    // TODO figure this out
+    ProxyState.sandboxSpells = res.data.map(s => new SandboxSpell(s))
+  }
+}
+
+export const sandboxService = new SandboxService()
